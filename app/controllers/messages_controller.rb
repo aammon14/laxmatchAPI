c95d2682dae46191c8547adf68f3c84dfce9a2f5
Andrew Ammon <andrewammon@ool-ad02026a.dyn.optonline.net>
2018-03-15 12:45:32 -0400

    user can create message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,32 @@
 class MessagesController < ApplicationController
 
-def index
-  messages = Message.all
-  render json: messages
-end
+  def index
+    messages = Message.all
+    render json: messages
+  end
 
-def show
-  message = Message.find(params[:id])
-  render json: message
-end
+  def show
+    message = Message.find(params[:id])
+    render json: message
+  end
+
+  def create
+    player_id = params[:player_id]
+    coach_id = params[:coach_id]
+    body = params[:body]
+
+    message = Message.create!({
+      player_id: player_id,
+      coach_id: coach_id,
+      body: body
+    })
+
+    if message
+      render json: message
+    else
+      render json: {err: 'Sorry there was an error'}
+    end
+  end
 
 end
 
